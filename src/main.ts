e12ac13e18d8155332acd038392d73360aec3250
VINNE7 <marcusv.ribas7@gmail.com>
2024-01-02 23:56:25 -0300

    feat: config main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  app.enableCors({
+    origin: configService.get<string>('ENDPOINT_CORS'),
+    methods: 'GET,POST,PUT,PATCH,DELETE',
+    credentials: true,
+  });
+  const port = configService.get<number>('NODE_API_PORT') || 3001;
+  await app.listen(port, '0.0.0.0');
+  Logger.log(
+    `${await app.getUrl()} - Enviroment: ${configService.get<string>(
+      'NODE_ENV',
+    )}`,
+    'Enviroment',
+  );
 }
 bootstrap();
